47cc82f4c56bcfb3f7e47072a04b379d3b3ff1b1
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-08-26 10:03:26 +0530

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+
+          {/* Home Page (the one with search button) */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          {/* Search Page (the search result page) */}
+          <Route exact path="/search">
+           <SearchPage />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      home page here
+      <div className="home__header">
+          <div className="home__headerLeft">
+        {/*link */}
+        {/* link */}
+        </div>
+        <div className="home_headerRight">
+        {/*link */}
+        {/* link */}
+        {/* menu */}
+        {/* avatar */}
+        </div>
+      </div>
+      <div className="home__body">
+
+      </div>
+    </div>
+  );
+}
