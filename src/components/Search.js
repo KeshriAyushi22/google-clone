9cc10dc213eb0f04d1a7a4273d0f58aaf6d36403
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-09-03 01:14:06 +0530

    search btn

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Mic } from '@material-ui/icons';
 import SearchIcon from '@material-ui/icons/Search';
 import "./Search.css"
+import { Button } from '@material-ui/core'
 
 export default function Search() {
     return (
@@ -10,6 +11,10 @@ export default function Search() {
                 <SearchIcon className="search__inputIcon" />
                 <input type="text" />
                 <Mic />
+            </div>
+            <div className="search__button">
+                <Button variant="outlined">Google Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
         </div>
     )
