44515184f66b79cb62f66f96c47adacf86c4021c
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-09-02 20:47:54 +0530

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Apps } from "@material-ui/icons";
+import { Avatar } from "@material-ui/core";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home">
-      home page here
       <div className="home__header">
-          <div className="home__headerLeft">
-        {/*link */}
-        {/* link */}
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
         <div className="home_headerRight">
-        {/*link */}
-        {/* link */}
-        {/* menu */}
-        {/* avatar */}
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <Apps />
+          <Avatar />
         </div>
       </div>
       <div className="home__body">
-
+          <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"  alt=""/>
       </div>
     </div>
   );
